8ecc0a3b7c227821e060e1292ef53c29b7a2aad5
pug <141905308+kiki9484@users.noreply.github.com>
2023-10-29 15:33:00 +0900

    Create 라빈 카프.java

---
+++ 알고리즘/문자열/라빈 카프.java
@@ -0,0 +1,30 @@
+public static int hash(String s){
+	int ret = 0;
+	for(int i = 0; i < s.length(); i++){
+	  char ch = s.charAt(i);
+	  ret = (ret * base + ch) % mod;
+	}
+	return ret;
+}
+    
+public static int rabinKarp(){
+	int n = H.length();
+	int m = N.length();
+	if(n < m) return 0;
+
+	int hashH = hash(H.substring(0, m));
+	int hashN = hash(N);
+	int first = 1;
+
+	for(int i = 0; i < m - 1; i++)
+    first = (first * base) % mod;
+
+	for(int i = 0; i <= n - m; i++){
+    if(hashN == hashH && H.substring(i, i + m).equals(N)) return 1;
+    if(i + m < n){
+        hashH = (hashH - ((H.charAt(i) * first) % mod) + mod) % mod;
+        hashH = ((hashH * base) % mod + H.charAt(i + m)) % mod;    
+		}
+	}
+	return 0;
+}
